483dfbbc340b46f1085d21563ec5b3f59b615fc2
rauatorres <rauatorres@gmail.com>
2023-09-15 22:07:43 -0300

    setando o server para escutar na mesma porta do client

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const {app} = require('./server/configs.js')
+const dotenv = require('dotenv')
 
-app.listen(5000, ()=>{
-    console.log('Server started successfully! Listening on port 5000!')
+const port = process.env.PORT || 3000
+
+app.listen(port, ()=>{
+    console.log(`Server started successfully! Listening on port ${port}!`)
 })
